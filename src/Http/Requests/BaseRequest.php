ea364af5e340c192631496887ab8b326eb4ffc60
Pavan Kataria <kataria.pavan@gmail.com>
2018-10-21 23:30:37 +0100

    Adds failedValidation handler for laravel 5.5+

--- src/Http/Requests/BaseRequest.php
+++ src/Http/Requests/BaseRequest.php
@@ -10,6 +10,8 @@ namespace PavanKataria\BoilerplateApi\Http\Requests;
 
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Response;
 
 /**
@@ -36,5 +38,12 @@ class BaseRequest extends FormRequest
             'errors' => $transformedErrorMessages
 //                ]
         ], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+
+    public function failedValidation(Validator $validator) {
+        //write your bussiness logic here otherwise it will give same old JSON response
+        dump($validator->errors()->messages());
+        throw new HttpResponseException($this->response($validator->errors()->messages()), 422);
     }
 }
